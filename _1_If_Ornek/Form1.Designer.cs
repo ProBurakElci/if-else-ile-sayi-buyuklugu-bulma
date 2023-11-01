ec6eb6bc55bd0a77d6085bda8c9f962ac5a3d4a0
Burak Elçi <burakelci12@gmail.com>
2023-11-01 13:07:06 +0300

    Proje dosyası ekle.

---
+++ _1_If_Ornek/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace _1_If_Ornek
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSayi1 = new System.Windows.Forms.TextBox();
+            this.txtSayi2 = new System.Windows.Forms.TextBox();
+            this.txtSayi3 = new System.Windows.Forms.TextBox();
+            this.btnBul = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 47);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "1.SAYI";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 100);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "2.SAYI";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 149);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "3.SAYI";
+            // 
+            // txtSayi1
+            // 
+            this.txtSayi1.Location = new System.Drawing.Point(129, 43);
+            this.txtSayi1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtSayi1.Name = "txtSayi1";
+            this.txtSayi1.Size = new System.Drawing.Size(132, 22);
+            this.txtSayi1.TabIndex = 3;
+            // 
+            // txtSayi2
+            // 
+            this.txtSayi2.Location = new System.Drawing.Point(129, 91);
+            this.txtSayi2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtSayi2.Name = "txtSayi2";
+            this.txtSayi2.Size = new System.Drawing.Size(132, 22);
+            this.txtSayi2.TabIndex = 4;
+            // 
+            // txtSayi3
+            // 
+            this.txtSayi3.Location = new System.Drawing.Point(129, 140);
+            this.txtSayi3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtSayi3.Name = "txtSayi3";
+            this.txtSayi3.Size = new System.Drawing.Size(132, 22);
+            this.txtSayi3.TabIndex = 5;
+            // 
+            // btnBul
+            // 
+            this.btnBul.Location = new System.Drawing.Point(129, 193);
+            this.btnBul.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnBul.Name = "btnBul";
+            this.btnBul.Size = new System.Drawing.Size(100, 28);
+            this.btnBul.TabIndex = 6;
+            this.btnBul.Text = "BUL";
+            this.btnBul.UseVisualStyleBackColor = true;
+            this.btnBul.Click += new System.EventHandler(this.btnBul_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(324, 262);
+            this.Controls.Add(this.btnBul);
+            this.Controls.Add(this.txtSayi3);
+            this.Controls.Add(this.txtSayi2);
+            this.Controls.Add(this.txtSayi1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSayi1;
+        private System.Windows.Forms.TextBox txtSayi2;
+        private System.Windows.Forms.TextBox txtSayi3;
+        private System.Windows.Forms.Button btnBul;
+    }
+}
+
---
+++ _1_If_Ornek/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1_If_Ornek
+{
+    public partial class Form1 : Form
+    {
+        // SORU : Girilen 3 sayıdan hangisi en büyük, hangisi en küçük hangisi ortancadır , bulan programı yazınız
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBul_Click(object sender, EventArgs e)
+        {
+            int sayi1, sayi2, sayi3, enBuyuk, enKucuk, ortanca;
+            if (txtSayi1.Text == "" || txtSayi2.Text == "" || txtSayi3.Text == "")
+            {
+                MessageBox.Show("Lütfen Bütün sayıları giriniz");
+             }
+            else {
+                sayi1 = Convert.ToInt32(txtSayi1.Text);
+                sayi2 = Convert.ToInt32(txtSayi2.Text);
+                sayi3 = Convert.ToInt32(txtSayi3.Text);
+
+                if (sayi1 > sayi2 && sayi1 > sayi3)
+                {
+                    enBuyuk = sayi1;
+
+                } else if (sayi2 > sayi1 && sayi2 > sayi3)
+                {
+                    enBuyuk = sayi2;
+                }
+                else
+                {
+                    enBuyuk = sayi3;
+                }
+                //-----------------------------------------------------
+                if (sayi1 < sayi2 && sayi1 < sayi3)
+                {
+                    enKucuk = sayi1;
+
+                } else if (sayi2 < sayi1 && sayi2 < sayi3)
+                {
+                    enKucuk = sayi2;
+                }
+                else
+                {
+                    enKucuk = sayi3;
+                }
+                //-----------------------------------------------------
+
+                if (sayi1 < enBuyuk && sayi1 > enKucuk)
+                {
+                    ortanca = sayi1;
+                }
+                else if (sayi2 < enBuyuk && sayi2 > enKucuk)
+                {
+                    ortanca = sayi2;
+                }
+                else
+                {
+                    ortanca = sayi3;
+                }
+                MessageBox.Show("EN BÜYÜK : " + enBuyuk + "\nORTANCA : " + ortanca + "\nEN KÜÇÜK : " + enKucuk);
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
